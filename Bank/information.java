c6fa20253331327d99d468bf115a98644365a3e9
trembleyluke <42677977+trembleyluke@users.noreply.github.com>
2019-03-21 19:52:06 -0700

    Add files via upload

---
+++ Bank/information.java
@@ -0,0 +1,12 @@
+public class information {
+    protected String name;
+    protected int accountNum;
+    protected double balance;
+    protected int expiration;
+    public information(String newName,int newAccountNum,double newBalance, int newExpiration){
+        name = newName;
+        accountNum = newAccountNum;
+        balance = newBalance;
+        expiration = newExpiration;
+    }
+}
